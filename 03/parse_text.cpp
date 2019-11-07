cb3f8c0100866212d7a4d3c07cbe1d7f838a5fc6
anton <antony.shadrin@gmail.com>
2019-11-07 20:31:33 +0300

    in 03 create run target

---
+++ 03/parse_text.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include "parser.h"
+
+// пишет токен и какому типу он соответствует
+void token_func(const token& tkn)
+{
+	std::cout << tkn.str;
+	if (tkn.type == NUMBER)
+		std::cout << " it's NUMBER token\n";
+	if (tkn.type == STRING)
+		std::cout << " it's STRING token\n";
+}
+
+// объ€вл€ет начало разбора строки
+void start_func()
+{
+	std::cout << "START!\n";
+}
+
+// объ€вл€ет конец разбора строки
+void end_func()
+{
+	std::cout << "END!\n";
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error! Need two arguments: parser_text.exe \"expression\"\n");
+		return 1;
+	}
+	std::string line(argv[1]);
+
+	// вызов с функци€ми
+	std::list<token> result = parse(line, token_func, start_func, end_func);
+	return 0;
+}
