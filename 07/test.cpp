ab0d918f2e3a765ae9c4c5ebdfcdcd09ea046dd2
anton <antony.shadrin@gmail.com>
2019-12-11 12:43:18 +0300

    delete .h and changed logic

--- 07/test.cpp
+++ 07/test.cpp
@@ -1,4 +1,88 @@
-#include "format.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <sstream>
+#include <stdlib.h>
+
+template<typename Arg>
+void format_sub(std::vector<std::string>& strs, Arg&& arg)
+{
+	std::stringstream ss;
+	ss << (std::forward<Arg>(arg));
+	strs.push_back(ss.str());
+}
+
+template<typename Arg, typename ... Args>
+void format_sub(std::vector<std::string>& strs, Arg&& arg, Args&& ... args)
+{
+	std::stringstream ss;
+	ss << (std::forward<Arg>(arg));
+	strs.push_back(ss.str());
+	format_sub(strs, std::forward<Args>(args) ...);
+}
+
+template<typename Arg, typename ... Args>
+std::string format(const std::string& str, Arg&& arg, Args&& ... args)
+{
+	// argument promotion
+	std::vector<std::string> strs;
+	format_sub(strs, std::forward<Arg>(arg), std::forward<Args>(args) ...);
+
+	// formatting string
+	std::string res;
+	std::string digit;
+	bool open_bracket = false;
+	for (size_t i = 0; i < str.size(); ++i)
+	{
+		char ch = str[i];
+		if (open_bracket)
+		{
+			if (isdigit(ch))
+			{
+				digit += ch;
+			}
+			else
+			{
+				if (ch == '}' && !digit.empty())
+				{
+					int num = atoi(digit.c_str());
+					if (num < static_cast<int>(strs.size()))
+					{
+						res += strs[num];
+					}
+					else
+					{
+						throw std::runtime_error("Expected more arguments.");
+					}
+					digit.clear();
+					open_bracket = false;
+				}
+				else
+				{
+					throw std::runtime_error("Expected number in brackets {...}.");
+				}
+			}
+		}
+		else
+		{
+			if (ch == '{')
+			{
+				open_bracket = true;
+			}
+			else
+			{
+				res += ch;
+			}
+		}
+	}
+	if (open_bracket)
+		throw std::runtime_error("Expected closed bracket }.");
+
+	return res;
+}
+
+
 
 bool test1()
 {
